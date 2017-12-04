f80e2c5f78d728775870900552a5e4a60f59919a
McDoyen <mrpoloh@yahoo.co.uk>
2017-12-04 11:08:15 +0300

    Widget structure

---
+++ src/components/Camera.ts
@@ -0,0 +1,8 @@
+import { Component } from "react";
+
+export class Camera extends Component<{}, {}> {
+
+    constructor(props: any) {
+        super(props);
+    }
+}
---
+++ src/components/CameraContainer.ts
@@ -0,0 +1,8 @@
+import { Component, createElement } from "react";
+
+export default class CameraContainer extends Component<{}> {
+   
+    constructor(props: any) {
+        super(props);
+    }
+}
